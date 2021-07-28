cd6897a03147dfa6ee142aeade5e8885d7828857
kjmatt91 <kjmatt91@gmail.com>
2021-07-27 22:32:53 -0400

    Got all tests but one to pass

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function validateLineup(lineup) {
     let salary = lineup.reduce((acc, player) => acc + player.salary,0)
     let teamRule = true
     let gameRule = true
+
+            // too many players = >9 too few = <9     'OF' !== 3 (false) 
+            if(lineup.length > 9 || lineup.length < 9) {
+                return false
+            }
+    
     for(let i = 0; i < lineup.length; i++) {
         if(lineup.filter(player => player.teamId === lineup[i].teamId).length > 2) {
             teamRule = false
@@ -10,13 +16,17 @@ function validateLineup(lineup) {
         if(lineup.filter(player => player.gameId === lineup[i].gameId).length > 3) {
             gameRule = false
         }
+        // if(lineup.filter(player => player.position))
     }
+
+
 
 
 return salary < 45000 && teamRule && gameRule
 }
 
-// 3) Lineups may not contain more than 3 players from a single game
+const outfield = 3
+
 // 4) Lineups must contain exactly 3 players with the position of 'OF' and must also contain exactly 1 player from each of the following positions: 'P', 'C', '1B', '2B', '3B', 'SS'
 
 module.exports = validateLineup
