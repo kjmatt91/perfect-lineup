04d83421bfcf19824f8cfdcf96f144e237271365
kjmatt91 <kjmatt91@gmail.com>
2021-07-29 00:02:12 -0400

    Setup debugger and got all tests to pass except test 1 now...

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-// takes in array of objects & returns boolean
 function validateLineup(lineup) {
-    let salary = lineup.reduce((acc, player) => acc + player.salary,0)
-    let teamRule = true
-    let gameRule = true
+  // let salary = lineup.reduce((acc, player) => acc + player.salary, 0)
+  let salary = true
+  let length = true
+  let teamRule = true
+  let gameRule = true
+  let outfield = true
+  let outfieldPos = 0
+  let otherPositions = true
+  let position = 0
 
-            // too many players = >9 too few = <9     'OF' !== 3 (false) 
-            if(lineup.length > 9 || lineup.length < 9) {
-                return false
-            }
-    
-    for(let i = 0; i < lineup.length; i++) {
-        if(lineup.filter(player => player.teamId === lineup[i].teamId).length > 2) {
-            teamRule = false
-        }
-        if(lineup.filter(player => player.gameId === lineup[i].gameId).length > 3) {
-            gameRule = false
-        }
-        // if(lineup.filter(player => player.position))
+
+  for (let i = 0; i < lineup.length; i++) {
+    if (lineup.salary > 45000) {
+      salary = false
+    }
+    if (lineup.length > 9 || lineup.length < 9) {
+      length = false
     }
 
+    if (lineup.filter(player => player.teamId === lineup[i].teamId).length > 2) {
+      teamRule = false
+    }
+    if (lineup.filter(player => player.gameId === lineup[i].gameId).length > 3) {
+      gameRule = false
+    }
+    if (lineup.filter(player => player.position[i] === 'OF')) {
+      outfieldPos = outfieldPos++
+      if (outfieldPos !== 3) {
+        outfield = false
+      }
+    }
+    if (lineup.filter(player => player.position[i] !== 'OF')) {
+      position = position++
+      if (position !== 1) {
+        otherPositions = false
+      }
+    }
+  }
 
-
-
-return salary < 45000 && teamRule && gameRule
+  return length && salary /* < 45000 */&& teamRule && gameRule && outfield && otherPositions
 }
-
-const outfield = 3
-
-// 4) Lineups must contain exactly 3 players with the position of 'OF' and must also contain exactly 1 player from each of the following positions: 'P', 'C', '1B', '2B', '3B', 'SS'
 
 module.exports = validateLineup
